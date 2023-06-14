0933417bc74b60e259e08527dd63ea2d96075fec
ocrastroav <134004463+ocrastroav@users.noreply.github.com>
2023-06-13 19:06:39 -0600

    Update cd-hit.sh
    Script más general y que para la ejecución debes dar los directorios

--- cd-hit.sh
+++ cd-hit.sh
@@ -1,7 +1,23 @@
 #!/bin/bash
 
-# Obtener la lista de carpetas en el directorio actual
-folders=$(find . -maxdepth 1 -type d ! -path .)
+# Verificar que se proporcionaron todas las rutas necesarias
+if [ $# -ne 4 ]; then
+  echo "Se requieren 4 argumentos:"
+  echo "1. Ruta del directorio principal"
+  echo "2. Ruta de VIBRANT"
+  echo "3. Ruta de VirFinder"
+  echo "4. Ruta de VirSorter"
+  exit 1
+fi
+
+# Asignar los argumentos a variables
+main_directory=$1
+vibrant_directory=$2
+virfinder_directory=$3
+virsorter_directory=$4
+
+# Obtener la lista de carpetas en el directorio principal
+folders=$(find "$main_directory" -maxdepth 1 -type d ! -path "$main_directory")
 
 # Iterar sobre cada carpeta
 for folder in $folders; do
@@ -12,13 +28,13 @@ for folder in $folders; do
   cd "$folder"
 
   # Enlace simbólico a muestra-VIBRANT.fasta
-  ln -s "/mnt/4TB/VIBRANT/VIBRANT_${number}-contigs/VIBRANT_phages_${number}-contigs/${number}-contigs.phages_combined.fna" "${number}-VIBRANT.fasta"
+  ln -s "$vibrant_directory/VIBRANT_${number}-contigs/VIBRANT_phages_${number}-contigs/${number}-contigs.phages_combined.fna" "${number}-VIBRANT.fasta"
 
   # Enlace simbólico a muestra-virfinder.fasta
-  ln -s "/mnt/4TB/VirFinder/listas/${number}-viral-virfinder.fa" "${number}-virfinder.fasta"
+  ln -s "$virfinder_directory/${number}-viral-virfinder.fa" "${number}-virfinder.fasta"
 
   # Enlace simbólico a muestra-virsorter.fasta
-  ln -s "/mnt/4TB/virsorter2/virsorter-${number}/final-viral-combined.fa" "${number}-virsorter.fasta"
+  ln -s "$virsorter_directory/virsorter-${number}/final-viral-combined.fa" "${number}-virsorter.fasta"
 
   # Concatenar los archivos *.fasta en un archivo llamado "number-phages.fasta"
   cat *.fasta > "${number}-phages.fasta"
@@ -36,10 +52,9 @@ for folder in $folders; do
   cd-hit-est-2d -i "${number}-VIBRANT-virsorter.fasta" -i2 "${number}-virfinder.fasta" -o "${number}-only-virfinder" -c 0.95 -d 0 -M 0 -T 0
   cd-hit-est-2d -i "${number}-VIBRANT-virfinder.fasta" -i2 "${number}-virsorter.fasta" -o "${number}-only-virsorter" -c 0.95 -d 0 -M 0 -T 0
 
-    # Contar las líneas que contienen ">" en los archivos generados y guardar el resultado en "number-only-sw"
+  # Contar las líneas que contienen ">" en los archivos generados y guardar el resultado en "number-only-sw"
   grep -c ">" "${number}-only-VIBRANT" "${number}-only-virsorter" "${number}-only-virfinder" > "${number}-only-sw"
 
   # Regresar al directorio anterior
   cd ..
 done
-
